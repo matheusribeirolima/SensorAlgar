9cef0f455a22173a919beb838e81e53572908000
matheusribeirolima <matheus_ribeirolima@hotmail.com>
2017-08-19 19:04:29 -0300

    Versão 2
    Versão melhorada

--- app/src/main/java/com/example/musico/sensor/MainActivity.java
+++ app/src/main/java/com/example/musico/sensor/MainActivity.java
@@ -1,20 +1,37 @@
 package com.example.musico.sensor;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
-
-//    @Override
-//    public void onAttachedToWindow() {
-//        this.getWindow().setType(WindowManager.LayoutParams.TYPE_KEYGUARD_DIALOG);
-//        super.onAttachedToWindow();
-//    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {//verifica se a versão do S.O. é maior que o SDK 23
+            if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {//se não tiver permissão
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);//solicita a permissão com o codigo 0
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 0://solicitação da permissão com codigo 0 que se refere a gravação de arquivo
+                if (grantResults.length > 0 && grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "O app precisa da permissão para funcionar corretamente!", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/musico/sensor/ServiceSensor.java
+++ app/src/main/java/com/example/musico/sensor/ServiceSensor.java
@@ -26,55 +26,50 @@ public class ServiceSensor extends Service implements SensorEventListener {
     Sensor senAccelerometer;
     float x, y, z;
     DecimalFormat df =  new DecimalFormat("0.000");
-    //int contador = 0;
     long lastUpdate = 0;
-    //float last_x, last_y, last_z;
-    //static final int SHAKE_THRESHOLD = 400;
     float[] gravity =  new float[3];
     File file = null;
     File arq = null;
-    String data, hora;
+    String data;
     StringBuilder vals = new StringBuilder();
 
     @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
+    public int onStartCommand(Intent intent, int flags, int startId) {//executa quando o service for chamado
         senSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        senSensorManager.registerListener(this, senAccelerometer , SensorManager.SENSOR_DELAY_NORMAL);
+        senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);//objeto que vai escutar o acelerometro
+        senSensorManager.registerListener(this, senAccelerometer , SensorManager.SENSOR_DELAY_NORMAL);//seta intervalo de leitura
         new Thread(new Runnable() {
-            public void run(){
-                while(true){
+            public void run(){//thread para gravar em arquivo
+                while(true){//durante toda a execução da aplicação
                     try {
-                        Thread.sleep(10000);
-                        hora = new SimpleDateFormat("HH:mm:ss").format(new Date(System.currentTimeMillis()));
+                        Thread.sleep(10000);//em intervalos de 10 segundos
                         if(vals.length() != 0){
-                            gravarArquivo();
+                            gravarArquivo();//método para gravar em arquivo
                         }
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
                 }
             }
-        }).start();
+        }).start();//inicia thread
         return super.onStartCommand(intent, flags, startId);
     }
 
     public void gravarArquivo() {
-        String state = Environment.getExternalStorageState();
-        if (Environment.MEDIA_MOUNTED.equals(state)) {
+        String state = Environment.getExternalStorageState();//recebe o path do diretório
+        if (Environment.MEDIA_MOUNTED.equals(state)) {//se estiver ok o armazenamento
             try {
-                data = new SimpleDateFormat("dd-MMM-yyyy").format(new Date(System.currentTimeMillis()));
-                file = new File(Environment.getExternalStorageDirectory().toString() + File.separator + "Sensor");
-                file.mkdirs();
-                arq = new File(file, data+"Eixos.txt");
-                FileWriter fw = new FileWriter(arq, true);
-                fw.write(hora + "\n" + vals + "\n");
-                vals.setLength(0);
-                //contador = 0;
-                fw.flush();
-                fw.close();
-                MediaScannerConnection.scanFile(getApplicationContext(), new String[]{file.getAbsolutePath()}, null, null);
-                MediaScannerConnection.scanFile(getApplicationContext(), new String[]{arq.getAbsolutePath()}, null, null);
+                data = new SimpleDateFormat("dd-MMM-yyyy").format(new Date(System.currentTimeMillis()));//recebe data atual
+                file = new File(Environment.getExternalStorageDirectory().toString() + File.separator + "Sensor");//cria objeto para manipular arquivo no diretorio Sensor
+                file.mkdirs();//se não existe o diretório, cria-o
+                arq = new File(file, data+"Eixos.txt");//cria o arquivo com a data e nome Eixos caso não exista
+                FileWriter fw = new FileWriter(arq, true);//abre o arquivo para escrita
+                fw.write(vals.toString());//escreve nele os valores apendicionados lidos dos sensores
+                vals.setLength(0);//depois de gravar zera a variável
+                fw.flush();//limpa o objeto de escrita
+                fw.close();//fecha o objeto
+                MediaScannerConnection.scanFile(getApplicationContext(), new String[]{file.getAbsolutePath()}, null, null);//exibe o arquivo imediatamente ao alterá-lo
+                MediaScannerConnection.scanFile(getApplicationContext(), new String[]{arq.getAbsolutePath()}, null, null);//exibe o diretório imediatamente após cria-lo
             } catch (IOException e) {
                 Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
             }
@@ -88,14 +83,13 @@ public class ServiceSensor extends Service implements SensorEventListener {
     }
 
     @Override
-    public void onSensorChanged(SensorEvent event) {
-        Sensor mySensor = event.sensor;
-        long curTime = System.currentTimeMillis();
-        if ((curTime - lastUpdate) > 20) {
-            //long diffTime = (curTime - lastUpdate);
+    public void onSensorChanged(SensorEvent event) {//metodo que escuta o sensor quando este detectar mudança
+        Sensor mySensor = event.sensor;//objeto que recebe dados do sensor
+        long curTime = System.currentTimeMillis();//obtem o tempo atual em milisegundos
+        if ((curTime - lastUpdate) > 20) {//a cada 20 milisegundos
             lastUpdate = curTime;
-            if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-                final float ALPHA = (float) 0.8;
+            if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {//verifica o acelerômetro
+                final float ALPHA = (float) 0.8;//anula gravidade
                 gravity[0] = ALPHA * gravity[0] + (1 - ALPHA) * event.values[0];
                 gravity[1] = ALPHA * gravity[1] + (1 - ALPHA) * event.values[1];
                 gravity[2] = ALPHA * gravity[2] + (1 - ALPHA) * event.values[2];
@@ -103,17 +97,7 @@ public class ServiceSensor extends Service implements SensorEventListener {
                 x = event.values[0] - gravity[0];
                 y = event.values[1] - gravity[1];
                 z = event.values[2] - gravity[2];
-                vals.append(df.format(x)).append(";").append(df.format(y)).append(";").append(df.format(z)).append("\n");
-                //contador += 1;
-                /*
-                float speed = Math.abs(x + y + z - last_x - last_y - last_z) / diffTime * 10000;
-                if (speed > SHAKE_THRESHOLD) {
-                    Toast.makeText(this, "Mexeu", Toast.LENGTH_SHORT).show();
-                }
-                last_x = x;
-                last_y = y;
-                last_z = z;
-                */
+                vals.append(df.format(x)).append(";").append(df.format(y)).append(";").append(df.format(z)).append("\r\n");//quebra de linha para o Windows
             }
         }
     }
